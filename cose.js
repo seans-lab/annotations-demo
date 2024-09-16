103a0e87728ad0e8e09679ecdbe57e64c3974fd6
Sean <41353165+seans-lab@users.noreply.github.com>
2024-09-17 09:38:16 +1000

    Delete cose.js

--- cose.js
+++
@@ -1,1 +0,0 @@
-``` const express = require('express');const bodyParser = require('body-parser');const fs = require('fs');const path = require('path'); const app = express();const port = 3000;const dataFilePath = path.join(__dirname, 'data.json');// Middleware to parse JSON bodiesapp.use(bodyParser.json());```
